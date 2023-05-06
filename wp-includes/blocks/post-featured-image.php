d0c50ffda454b0897c7dfa3702a43540b3915320
Jhonny A. Gomez S <jhonny@jhoncytech.com>
2023-05-06 18:13:17 +0200

    Version 6.0.3

--- wp-includes/blocks/post-featured-image.php
+++ wp-includes/blocks/post-featured-image.php
@@ -19,7 +19,9 @@ function render_block_core_post_featured_image( $attributes, $content, $block ) 
 	}
 	$post_ID = $block->context['postId'];
 
-	$featured_image = get_the_post_thumbnail( $post_ID );
+	$size_slug      = isset( $attributes['sizeSlug'] ) ? $attributes['sizeSlug'] : 'post-thumbnail';
+	$post_title     = trim( strip_tags( get_the_title( $post_ID ) ) );
+	$featured_image = get_the_post_thumbnail( $post_ID, $size_slug, array( 'alt' => $post_title ) );
 	if ( ! $featured_image ) {
 		return '';
 	}
--- wp-mail.php
+++ wp-mail.php
@@ -62,7 +62,7 @@ if ( false === $count ) {
 
 if ( 0 === $count ) {
 	$pop3->quit();
-	wp_die( __( 'There doesn&#8217;t seem to be any new mail.' ) );
+	wp_die( __( 'There does not seem to be any new mail.' ) );
 }
 
 // Always run as an unauthenticated user.
